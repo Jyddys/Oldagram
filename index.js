c32b61146aa001352f0c77bfee34d14a329dde83
jyddys1 <justus.sydanmaa@hotmail.com>
2022-11-16 18:26:00 +0200

    Using more functions

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const posts = [
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: "21,492"
+        likes: Number(2000),
+        id: "001",
+        isLiked: false
     },
     {
         name: "Gustave Courbet",
@@ -16,7 +18,9 @@ const posts = [
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: "12,502"
+        likes: Number(1200),
+        id: "002",
+        isLiked: false
     },
         {
         name: "Joseph Ducreux",
@@ -25,42 +29,75 @@ const posts = [
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: "15,137"
+        likes: Number(1500),
+        id: "003",
+        isLiked: false
     }
 ]
 
+document.addEventListener('click', function(e) {
+
+    if(e.target.dataset.like){
+        handleLikeClick(e.target.dataset.like)
+    }
+})
+
+// Like post
+function handleLikeClick(likeId){
+
+    const targetLikeObj = posts.filter(function(post){
+        return post.id === likeId
+    })[0]
+
+    if(targetLikeObj.isLiked) {
+        targetLikeObj.likes--
+    } else {
+        targetLikeObj.likes++
+    }
+
+targetLikeObj.isLiked = !targetLikeObj.isLiked
+render()
+}
 
 
-const mainEl = document.getElementById("main")
+// renders html
+function getFeedHtml() {
 let html = ""
-for (let i = 0; i < posts.length; i++) {
+
+posts.forEach(function(post){
     html += `
     <main id="main">
     <section class="profile-bar">
-            <img class="avatar" src="${posts[i].avatar}">
+            <img class="avatar" src="${post.avatar}">
         <div class="text-box">
-            <p class="user-full-name">${posts[i].username}</p>
-            <p class="user-location">${posts[i].location}</p>
+            <p class="user-full-name">${post.username}</p>
+            <p class="user-location">${post.location}</p>
         </div>
     </section>
 
     <section class="big-image">
-        <img id="user-image" src=${posts[i].post}>
+        <img id="user-image" src=${post.post}>
     </section>
 
     <section class="symbols">
         <div class="symbols-box">
-            <img id="like" src="images/icon-heart.png">
+            <img id="like" src="images/icon-heart.png" data-like="${post.id}">
             <img class="comment" src="images/icon-comment.png">
             <img class="share" src="images/icon-dm.png">
     </div>
-        <p class="likes">${posts[i].likes} likes</p>
+        <p class="likes">${post.likes} likes</p>
         <p class="username-caption">
-            <span class="bold-text">${posts[i].username}</span> 
-            ${posts[i].comment}</p>
+            <span class="bold-text">${post.username}</span> 
+            ${post.comment}</p>
     </section>
 </main>
     `
+})
+return html
 }
 
-mainEl.innerHTML = html;
+function render() {
+    document.getElementById("main").innerHTML = getFeedHtml()
+}
+
+render(posts)
